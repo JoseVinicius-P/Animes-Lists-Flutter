47726ab2c638f48bd77a01b7d2918b4fede21c10
JoseVinicius-P <josevpf123@gmail.com>
2023-04-16 17:35:06 -0300

    style: improving the google button

--- lib/modules/login/widgets/google_button.dart
+++ lib/modules/login/widgets/google_button.dart
@@ -21,42 +21,62 @@ class GoogleButton extends StatelessWidget {
         height: 50,
         width: 200,
         decoration: BoxDecoration(
-            borderRadius: const BorderRadius.all(
-              Radius.circular(25),
-            ),
-            border: Border.all(color: Colors.white.withOpacity(0.7)),
-            boxShadow: [
-              BoxShadow(
-                color: MyColors.primaryColor.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 50,
-                offset: const Offset(30, 10),
-              ),
-              BoxShadow(
-                color: MyColors.accentColor.withOpacity(0.5),
-                spreadRadius: 1,
-                blurRadius: 50,
-                offset: const Offset(-30, -10),
-              )
-            ]
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Row(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(
-                image: AssetImage('assets/images/icon_google.png'),
-                fit: BoxFit.cover,
-              ),
-              const SizedBox(width: 5),
-              Text(
-                'Entrar com o google',
-                style: theme.textTheme.labelMedium,
-                textAlign: TextAlign.center,
-              ),
+          borderRadius:BorderRadius.all(
+            Radius.circular(25),
+          ),
+          border: Border.all(
+            width: 1,
+            style: BorderStyle.solid,
+            color: Colors.transparent,
+          ),
+          gradient: LinearGradient(
+            colors: [
+              MyColors.primaryColor,
+              MyColors.accentColor,
             ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: MyColors.primaryColor.withOpacity(0.2),
+              spreadRadius: 0.1,
+              blurRadius: 15,
+              offset: const Offset(-5, -5),
+            ),
+            BoxShadow(
+              color: MyColors.accentColor.withOpacity(0.2),
+              spreadRadius: 0.1,
+              blurRadius: 15,
+              offset: const Offset(5, 5),
+            )
+          ]
+        ),
+        child: SizedBox.expand(
+          child: Container(
+            decoration: BoxDecoration(
+              color: MyColors.backgroundColor.withOpacity(0.9),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Image(
+                    image: AssetImage('assets/images/icon_google.png'),
+                    fit: BoxFit.cover,
+                  ),
+                  const SizedBox(width: 5),
+                  Text(
+                    'Entrar com o google',
+                    style: theme.textTheme.labelMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
       ),
