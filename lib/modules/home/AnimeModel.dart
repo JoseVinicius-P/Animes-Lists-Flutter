abbc9a93e9bd0dc1079cb8fe8136c2e4bcafa85b
JoseVinicius-P <josevpf123@gmail.com>
2023-04-29 13:41:10 -0300

    feat: Implements Anime Model Class

--- lib/modules/home/AnimeModel.dart
+++ lib/modules/home/AnimeModel.dart
@@ -1,3 +1,26 @@
 class AnimeModel{
 
+  int id;
+  String title, main_picture, synopsis, status;
+  DateTime start_date;
+
+  AnimeModel({
+    required this.id,
+    required this.title,
+    required this.main_picture,
+    required this.synopsis,
+    required this.status,
+    required this.start_date,
+  });
+
+  factory AnimeModel.fromJson(Map<String, dynamic> json) =>
+    AnimeModel(
+      id: json['id'],
+      title: json['title'],
+      main_picture: json['main_picture'],
+      synopsis: json['synopsis'],
+      status: json['status'],
+      start_date: json['start_date'],
+    );
+
 }
