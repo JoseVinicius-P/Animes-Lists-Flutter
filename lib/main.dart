25c68b2db561166d2f86b84c3504c0c29ad71e01
JoseVinicius-P <josevpf123@gmail.com>
2023-05-06 22:56:44 -0300

    feat: Adding Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:anime_lists/app_module.dart';
 import 'package:anime_lists/app_widget.dart';
+import 'package:anime_lists/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
-void main() {
-  return runApp(ModularApp(module: AppModule(), child: AppWidget()));
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(ModularApp(module: AppModule(), child: AppWidget()));
 }
