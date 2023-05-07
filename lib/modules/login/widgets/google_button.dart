f0a683b3083634fd69ebdc9b01ae3fb831d9513a
JoseVinicius-P <josevpf123@gmail.com>
2023-05-07 09:58:52 -0300

    feat: adding Google auth

--- lib/modules/login/widgets/google_button.dart
+++ lib/modules/login/widgets/google_button.dart
@@ -1,11 +1,30 @@
 import 'package:anime_lists/shared/utilities/my_colors.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class GoogleButton extends StatelessWidget {
   const GoogleButton({
     super.key,
   });
+
+  Future<UserCredential> signInWithGoogle() async {
+    // Trigger the authentication flow
+    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+    // Obtain the auth details from the request
+    final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+    // Create a new credential
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    // Once signed in, return the UserCredential
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 
   void toListsModule(){
     Modular.to.navigate('/home/');
@@ -16,7 +35,7 @@ class GoogleButton extends StatelessWidget {
     var theme = Theme.of(context);
 
     return InkWell(
-      onTap: () => toListsModule(),
+      onTap: () => signInWithGoogle(),
       child: Container(
         height: 50,
         width: 200,
