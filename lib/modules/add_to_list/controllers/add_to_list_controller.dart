1e8bffa236f901cd970575ea1492c2cb60d6f164
JoseVinicius-P <josevpf123@gmail.com>
2023-05-13 14:07:25 -0300

    feat: Adding dependence injection for fetchLists

--- lib/modules/add_to_list/controllers/add_to_list_controller.dart
+++ lib/modules/add_to_list/controllers/add_to_list_controller.dart
@@ -1,12 +1,21 @@
+import 'package:anime_lists/modules/add_to_list/interfaces/i_list_model.dart';
+import 'package:anime_lists/modules/add_to_list/interfaces/i_list_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AddToListController implements Disposable{
   final textController = TextEditingController();
+  final IListService listService;
+
+  AddToListController(this.listService);
 
   @override
   void dispose() {
     textController.dispose();
+  }
+
+  Future<List<IListModel>> fetchLists() async{
+    return await listService.fetchLists();
   }
 
 }
