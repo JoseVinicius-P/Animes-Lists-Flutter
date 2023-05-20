36e177e746fa1947a4a6e7f4a6d6a5dcb8d7bf49
JoseVinicius-P <josevpf123@gmail.com>
2023-05-19 23:18:36 -0300

    feat: adding animes quantity in List

--- lib/shared/interfaces/i_list_model.dart
+++ lib/shared/interfaces/i_list_model.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract class IListModel{
   late String name, id;
+  late int qt_animes;
 
   IListModel setFromDocumentSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> docSnapshot);
   void setName(String name);
--- lib/shared/models/list_model.dart
+++ lib/shared/models/list_model.dart
@@ -4,6 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ListModel implements IListModel{
   @override
   late String name, id;
+  @override
+  late int qt_animes;
 
   ListModel({
     required this.name,
@@ -15,6 +17,7 @@ class ListModel implements IListModel{
   @override
   ListModel setFromDocumentSnapshot(QueryDocumentSnapshot<Map<String, dynamic>> docSnapshot){
     name = docSnapshot['name'];
+    qt_animes = docSnapshot['qt_animes'];
     id = docSnapshot.id;
     return this;
   }
@@ -22,6 +25,7 @@ class ListModel implements IListModel{
   @override
   void setName(String name) {
     this.name = name;
+    qt_animes = 0;
     id = "create";
   }
 }
