ead732cfcc085c8d57ab06d99406855b4398e467
JoseVinicius-P <josevpf123@gmail.com>
2023-05-31 22:58:21 -0300

    style: Improving layout according to orientation

--- lib/modules/login/pages/login_page.dart
+++ lib/modules/login/pages/login_page.dart
@@ -10,101 +10,98 @@ class LoginPage extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
 
-    return OrientationLayoutBuilder(
-      portrait: (context) => getScaffold(true, context),
-      landscape: (context) => getScaffold(false, context),
-    );
-  }
-
-  Widget getScaffold(bool isPortrait, BuildContext context){
-    var theme = Theme.of(context);
-    return Scaffold(
-        body: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              color: MyColors.backgroundColor,
-            ),
-            Stack(
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Container(
-                      child: isPortrait ?
-                        const Image(image: AssetImage('assets/images/capa_login.png')) :
-                        const Expanded(child: Image(image: AssetImage('assets/images/capa_login_landscape.png'))),
-                    ),
-                  ],
-                ),
-                SafeArea(
-                  child: Row(
-                    mainAxisAlignment: isPortrait ? MainAxisAlignment.center : MainAxisAlignment.end,
+    Widget getScaffold(bool isPortrait, BuildContext context){
+      var theme = Theme.of(context);
+      return Scaffold(
+          body: Stack(
+            children: [
+              Container(
+                width: double.infinity,
+                color: MyColors.backgroundColor,
+              ),
+              Stack(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
                     children: [
                       Container(
-                        child: !isPortrait ? SizedBox(width: 50.screenWidth,) : const SizedBox(),
+                        child: isPortrait ?
+                        const Image(image: AssetImage('assets/images/capa_login.png')) :
+                        const Expanded(child: Image(image: AssetImage('assets/images/capa_login_landscape.png'))),
                       ),
-                      Flexible(
-                        child: Column(
-                          mainAxisAlignment: isPortrait ? MainAxisAlignment.end : MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.all(Radius.circular(20)),
-                                  color: MyColors.backgroundColor.withOpacity(0.8)
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.all(5.0),
-                                child: AutoSizeText(
-                                  'Suas listas de animes',
-                                  style: theme.textTheme.titleMedium,
-                                  maxFontSize: 30,
-                                  minFontSize: 15,
-                                  maxLines: 1,
-                                  overflow: TextOverflow.ellipsis,
+                    ],
+                  ),
+                  SafeArea(
+                    child: Row(
+                      mainAxisAlignment: isPortrait ? MainAxisAlignment.center : MainAxisAlignment.end,
+                      children: [
+                        Flexible(
+                          child: Column(
+                            mainAxisAlignment: isPortrait ? MainAxisAlignment.end : MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.all(Radius.circular(20)),
+                                    color: MyColors.backgroundColor.withOpacity(0.8)
                                 ),
-                              ),
-                            ),
-                            Container(
-                              decoration: BoxDecoration(
-                                  borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
-                                  color: MyColors.backgroundColor.withOpacity(0.8)
-                              ),
-                              child: Padding(
-                                padding: const EdgeInsets.only(right: 3.0, bottom: 5.0, left: 5.0),
-                                child: SizedBox(
-                                  width: 200,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(5.0),
                                   child: AutoSizeText(
-                                    'Crie e gerencie listas com seus animes',
-                                    maxLines: 2,
-                                    softWrap: true,
-                                    textAlign: TextAlign.center,
-                                    style: theme.textTheme.titleSmall,
-                                    maxFontSize: 20,
-                                    minFontSize: 12,
+                                    'Suas listas de animes',
+                                    style: theme.textTheme.titleMedium!.copyWith(fontSize: 8.sw.roundToDouble()),
+                                    maxFontSize: 8.sw.roundToDouble(),
+                                    minFontSize: 4.sw.roundToDouble(),
+                                    maxLines: 1,
                                     overflow: TextOverflow.ellipsis,
                                   ),
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 30),
-                            GoogleButton(),
-                            Container(
-                              child: isPortrait ? SizedBox(height: 10.screenHeight) : const SizedBox(),
-                            ),
-                          ],
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: const BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20)),
+                                    color: MyColors.backgroundColor.withOpacity(0.8)
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(right: 3.0, bottom: 5.0, left: 5.0),
+                                  child: SizedBox(
+                                    width: 40.sw.roundToDouble(),
+                                    child: AutoSizeText(
+                                      'Crie e gerencie listas com seus animes',
+                                      maxLines: 2,
+                                      softWrap: true,
+                                      textAlign: TextAlign.center,
+                                      style: theme.textTheme.titleSmall!.copyWith(fontSize: 5.sw.roundToDouble()),
+                                      maxFontSize: 5.sw.roundToDouble(),
+                                      minFontSize: 2.3.sw.roundToDouble(),
+                                      overflow: TextOverflow.ellipsis,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(height: 30),
+                              GoogleButton(),
+                              Container(
+                                child: isPortrait ? SizedBox(height: 10.sh) : const SizedBox(),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        child: isPortrait ? const SizedBox(height: 0) : SizedBox(width: 25.screenWidth),
-                      ),
-                    ],
+                        Container(
+                          child: isPortrait ? const SizedBox(height: 0) : SizedBox(width: 25.sw),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        )
+                ],
+              ),
+            ],
+          )
+      );
+    }
+
+    return OrientationLayoutBuilder(
+      portrait: (context) => getScaffold(true, context),
+      landscape: (context) => getScaffold(false, context),
     );
   }
 }
--- lib/modules/login/widgets/google_button.dart
+++ lib/modules/login/widgets/google_button.dart
@@ -3,6 +3,7 @@ import 'package:anime_lists/shared/utilities/my_colors.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class GoogleButton extends StatelessWidget {
 
@@ -15,82 +16,91 @@ class GoogleButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    var buttonWidth = 50.sw.roundToDouble();
+    var buttonHeight = buttonWidth/4.roundToDouble();
 
-    return InkWell(
-      borderRadius: BorderRadius.circular(25),
-      //Se a authenticação estiver sendo feita o botão não funcionará
-      onTap: loginController.inAuthentication ? null : () async {
-        await loginController.signInWithGoogle();
-        loginController.toHomeModule();
-      },
-      child: Container(
-        height: 50,
-        width: 200,
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(
-            Radius.circular(25),
-          ),
-          border: Border.all(
-            width: 1,
-            style: BorderStyle.solid,
-            color: Colors.transparent,
-          ),
-          gradient: const LinearGradient(
-            colors: [
-              MyColors.primaryColor,
-              MyColors.accentColor,
-            ],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: MyColors.primaryColor.withOpacity(0.2),
-              spreadRadius: 0.1,
-              blurRadius: 15,
-              offset: const Offset(-5, -5),
-            ),
-            BoxShadow(
-              color: MyColors.accentColor.withOpacity(0.2),
-              spreadRadius: 0.1,
-              blurRadius: 15,
-              offset: const Offset(5, 5),
-            )
-          ]
-        ),
-        child: SizedBox.expand(
-          child: Container(
-            decoration: BoxDecoration(
-              color: MyColors.backgroundColor.withOpacity(0.9),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(10),
-              child: Row(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Image(
-                    image: AssetImage('assets/images/icon_google.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  const SizedBox(width: 5),
-                  Expanded(
-                    child: AutoSizeText(
-                      maxLines: 1,
-                      minFontSize: 12,
-                      'Entrar com o google',
-                      style: theme.textTheme.labelMedium,
-                      textAlign: TextAlign.center,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
+    Widget getGoogleButton(bool isPortrait){
+      return InkWell(
+        borderRadius: BorderRadius.circular(buttonHeight),
+        //Se a authenticação estiver sendo feita o botão não funcionará
+        onTap: loginController.inAuthentication ? null : () async {
+          await loginController.signInWithGoogle();
+          loginController.toHomeModule();
+        },
+        child: Container(
+          height: buttonHeight,
+          width: buttonWidth,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(
+                Radius.circular(buttonHeight),
+              ),
+              border: Border.all(
+                width: 1,
+                style: BorderStyle.solid,
+                color: Colors.transparent,
+              ),
+              gradient: const LinearGradient(
+                colors: [
+                  MyColors.primaryColor,
+                  MyColors.accentColor,
                 ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
+              boxShadow: [
+                BoxShadow(
+                  color: MyColors.primaryColor.withOpacity(0.2),
+                  spreadRadius: 0.1,
+                  blurRadius: 15,
+                  offset: const Offset(-5, -5),
+                ),
+                BoxShadow(
+                  color: MyColors.accentColor.withOpacity(0.2),
+                  spreadRadius: 0.1,
+                  blurRadius: 15,
+                  offset: const Offset(5, 5),
+                )
+              ]
+          ),
+          child: SizedBox.expand(
+            child: Container(
+              decoration: BoxDecoration(
+                color: MyColors.backgroundColor.withOpacity(0.9),
+                borderRadius: BorderRadius.circular(buttonHeight),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(buttonHeight/5.roundToDouble()),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Image(
+                      image: AssetImage('assets/images/icon_google.png'),
+                      fit: BoxFit.cover,
+                    ),
+                    const SizedBox(width: 5),
+                    Expanded(
+                      child: AutoSizeText(
+                        maxLines: 1,
+                        minFontSize: 2.5.sw.roundToDouble(),
+                        'Entrar com o google',
+                        style: theme.textTheme.labelMedium!.copyWith(fontSize: 5.sw.roundToDouble()),
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
+      );
+    }
+
+    return OrientationLayoutBuilder(
+      portrait: (context) => getGoogleButton(true),
+      landscape: (context) => getGoogleButton(false),
     );
   }
 }
