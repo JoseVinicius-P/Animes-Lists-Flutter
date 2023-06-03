4366d7fb3e6046f9759187611db8670b2868fda1
JoseVinicius-P <josevpf123@gmail.com>
2023-06-03 14:32:43 -0300

    feat: Adding status from Jikan api

--- lib/shared/utilities/status_interpreter.dart
+++ lib/shared/utilities/status_interpreter.dart
@@ -1,10 +1,18 @@
 class StatusInterpreter{
   static String getStatus(String oldStatus){
     switch(oldStatus){
+      case 'Finished':
       case 'finished_airing':
+      case 'Finished Airing':
         return 'Finalizado';
+      case 'Publishing':
       case 'currently_airing':
+      case 'Currently Airing':
         return 'Em exibição';
+      case 'On Hiatus':
+        return 'Em hiato';
+      case 'Discontinued':
+        return 'Descontinuado';
       default:
         return 'Ainda não foi exibido';
     }
