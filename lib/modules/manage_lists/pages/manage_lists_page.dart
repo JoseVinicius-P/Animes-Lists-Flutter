7925f919031bebd3637c3c0c7d58a72ff3af4aaa
JoseVinicius-P <josevpf123@gmail.com>
2023-06-09 17:21:15 -0300

    style: improving responsiveness

--- lib/modules/manage_lists/pages/manage_lists_page.dart
+++ lib/modules/manage_lists/pages/manage_lists_page.dart
@@ -4,6 +4,7 @@ import 'package:anime_lists/shared/interfaces/i_list_model.dart';
 import 'package:anime_lists/shared/utilities/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 class ManageListsPage extends StatefulWidget {
   const ManageListsPage({Key? key}) : super(key: key);
@@ -52,60 +53,76 @@ class _ManageListsPageState extends State<ManageListsPage> {
             color: MyColors.backgroundColor,
           ),
           SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: FutureBuilder<List<IListModel>>(
-                future: futureListModel,
-                builder: (context, snapshot){
-                  if(snapshot.hasData){
-                    return Column(
-                      children: [
-                        ReorderableListView(
-                          buildDefaultDragHandles: false,
-                          shrinkWrap: true,
-                          children: <Widget>[
-                            for (int i = 0; i < snapshot.data!.length; i += 1)
-                              Container(
-                                key: Key('$i'),
-                                decoration: const BoxDecoration(
-                                  color: MyColors.backgroundColor
-                                ),
-                                child: Row(
-                                  children: [
-                                    Text(
-                                      snapshot.data![i].name,
-                                      style: theme.textTheme.labelSmall!.copyWith(fontSize: 20),
-                                    ),
-                                    const Spacer(),
-                                    ReorderableDragStartListener(index:i ,child: Icon(Icons.drag_handle_rounded, color: Colors.white.withOpacity(0.5)),),
-                                    const SizedBox(height: 40),
-                                  ],
-                                ),
-                              ),
-                          ],
-                          onReorder: (int oldIndex, int newIndex) {
-                            setState(() {
-                              if (newIndex > oldIndex) {
-                                newIndex -= 1;
-                              }
-                              final item = snapshot.data!.removeAt(oldIndex);
-                              snapshot.data!.insert(newIndex, item);
-                            });
-                          },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Flexible(
+                  child: SingleChildScrollView(
+                    child: Container(
+                      constraints: BoxConstraints(
+                        maxWidth: 100.sw.roundToDouble(),
                       ),
-                      const SizedBox(height: 70),
-                      SaveButton(onTap: manageListController.savingInProgress ? null : () async {
-                        setState(() {
-                          manageListController.orderLists(oldOrder, snapshot.data!);
-                        });
-                      }),
-                    ],
-                  );
-                  }else{
-                    return const SizedBox();
-                  }
-                },
-              ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30.0),
+                        child: FutureBuilder<List<IListModel>>(
+                          future: futureListModel,
+                          builder: (context, snapshot){
+                            if(snapshot.hasData){
+                              return Column(
+                                children: [
+                                  ReorderableListView(
+                                    physics: const NeverScrollableScrollPhysics(),
+                                    buildDefaultDragHandles: false,
+                                    shrinkWrap: true,
+                                    children: <Widget>[
+                                      for (int i = 0; i < snapshot.data!.length; i += 1)
+                                        Container(
+                                          key: Key('$i'),
+                                          decoration: const BoxDecoration(
+                                            color: MyColors.backgroundColor
+                                          ),
+                                          child: Row(
+                                            children: [
+                                              Text(
+                                                snapshot.data![i].name,
+                                                style: theme.textTheme.labelSmall!.copyWith(fontSize: 20),
+                                              ),
+                                              const Spacer(),
+                                              ReorderableDragStartListener(index:i ,child: Icon(Icons.drag_handle_rounded, color: Colors.white.withOpacity(0.5)),),
+                                              const SizedBox(height: 40),
+                                            ],
+                                          ),
+                                        ),
+                                    ],
+                                    onReorder: (int oldIndex, int newIndex) {
+                                      setState(() {
+                                        if (newIndex > oldIndex) {
+                                          newIndex -= 1;
+                                        }
+                                        final item = snapshot.data!.removeAt(oldIndex);
+                                        snapshot.data!.insert(newIndex, item);
+                                      });
+                                    },
+                                ),
+                                const SizedBox(height: 70),
+                                SaveButton(onTap: manageListController.savingInProgress ? null : () async {
+                                  setState(() {
+                                    manageListController.orderLists(oldOrder, snapshot.data!);
+                                  });
+                                }),
+                              ],
+                            );
+                            }else{
+                              return const SizedBox();
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
